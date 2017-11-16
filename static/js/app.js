251075835adf4ae7ba7357e9cc2688aab05105b5
jiwo0 <kjwoo0921@naver.com>
2017-11-16 18:05:26 +0900

    registration fixed

--- static/js/app.js
+++ static/js/app.js
@@ -4,65 +4,21 @@ function step1() {
   $('#step1').css("opacity", "1");
   $('#step3').css("opacity", "0.5");
 }
+function onlyNumber(obj) {
+    $(obj).keyup(function(){
+         $(this).val($(this).val().replace(/[^0123456789-]/g,""));
+    });
+}
+function checkPasswordMatch() {
+    var password = $("#password").val();
+    var confirmPassword = $("#password_confirm").val();
 
-//
-// //회원가입 폼
-// $("#submit_button").click(function() {
-//     var id = $("#id").val()
-//     var password = $("#password").val()
-//     var name = $("#name").val()
-//     console.log(id+'='+password+"+"+name)
-//   var formData = {
-//     name:$name.val(),
-//     id:$id.val(),
-//     password:$password.val(),
-//     birthday:$birthday.val(),
-//     gender:$gender.val(),
-//     phoneNumber:$phoneNumber.val()
-//     };
-//   $("#sign_form").validate({
-//     rules: {
-//       name: {
-//         required: true,
-//       },
-//       password: {
-//         required: true,
-//       },
-//       password_confirm: {
-//         required: true,
-//         equalTo: "#password",
-//       },
-//       id: {
-//         required: true,
-//       },
-//       birthday: {
-//         required: true,
-//       },
-//       phoneNumber: {
-//         required: true
-//       }
-//     },
-//     messages: {
-//       name: "이름을 입력해 주세요",
-//       id: "아이디를 입력해 주세요",
-//       password: "암호를 입력해 주세요",
-//       confirm_password: {
-//         required: "암호를 한 번 더 입력해 주세요",
-//         equalTo: "암호가 일치하지 않습니다"
-//       },
-//       birthday: "생년월일을 입력해 주세요",
-//       phoneNumber: "전화번호를 입력해 주세요"
-//     }
-//   });
-//   $.ajax({
-//     type: "POST",
-//     url: "/register",
-//     cache: false,
-//     data: {"id":id, "password":password, "username": name},
-//     success: complete,
-//     error: onError
-//   });
-// });
+    if (password != confirmPassword)
+        $("#confirm-message").html("비밀번호가 맞지 않습니다");
+    else
+        $("#confirm-message").html("");
+
+}
 
 
 
