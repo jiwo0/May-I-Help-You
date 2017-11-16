764ecd70206f5fe2606f44e47cfe517a793e5b27
jiwo0 <kjwoo0921@naver.com>
2017-11-16 18:45:12 +0900

    registration fixed

--- static/js/app.js
+++ static/js/app.js
@@ -4,20 +4,29 @@ function step1() {
   $('#step1').css("opacity", "1");
   $('#step3').css("opacity", "0.5");
 }
+
 function onlyNumber(obj) {
-    $(obj).keyup(function(){
-         $(this).val($(this).val().replace(/[^0123456789-]/g,""));
-    });
+  $(obj).keyup(function() {
+    $(this).val($(this).val().replace(/[^0123456789-]/g, ""));
+  });
 }
+
 function checkPasswordMatch() {
-    var password = $("#password").val();
-    var confirmPassword = $("#password_confirm").val();
+  var password = $("#password").val();
+  var confirmPassword = $("#password_confirm").val();
 
-    if (password != confirmPassword)
-        $("#confirm-message").html("비밀번호가 맞지 않습니다");
-    else
-        $("#confirm-message").html("");
+  if (password != confirmPassword)
+    $("#confirm-message").html("비밀번호가 맞지 않습니다");
+  else
+    $("#confirm-message").html("");
 
+}
+function onSuccess(json, status) {
+  alert($.trim(json));
+}
+
+function onError(data, status) {
+  alert("error");
 }
 
 
@@ -30,43 +39,47 @@ function complete() {
   $('#step3').css("opacity", "1");
 }
 
-function onSuccess(json, status) {
-  alert($.trim(json));
-}
 
-function onError(data, status) {
-  alert("error");
-}
 
 function onLogin() {
-    var id = $("#id").val()
-    var password = $("#password").val()
-    console.log(id+'='+password+"+"+name)
-    $.ajax({
-        type: "POST",
-        url: "/login",
-        cache: false,
-        data: {"id":id, "password":password},
-        success: onSuccess,
-        error: onError
-    });
+  var id = $("#id").val()
+  var password = $("#password").val()
+  console.log(id + '=' + password + "+" + name)
+  $.ajax({
+    type: "POST",
+    url: "/login",
+    cache: false,
+    data: {
+      "id": id,
+      "password": password
+    },
+    success: onSuccess,
+    error: onFail
+  });
 }
 
 function goNext() {
-    var id = $("#id").val()
-    var password = $("#password").val()
-    var name = $("#name").val()
-    var password = $('#password').val()
-    var birthday = $('#birthday').val()
-    var gender = $('#gender').val()
-    var phoneNumber = $('#phoneNumber').val()
-    console.log(id+'='+password+"+"+name)
-    $.ajax({
-        type: "POST",
-        url: "/register",
-        cache: false,
-        data: {"id":id, "password":password, "username": name,"birthday":birthday,"gender":gender,"phoneNumber":phoneNumber},
-        success: complete,
-        error: onError
-    });
+  var id = $("#id").val()
+  var password = $("#password").val()
+  var name = $("#name").val()
+  var password = $('#password').val()
+  var birthday = $('#birthday').val()
+  var gender = $('#gender').val()
+  var phoneNumber = $('#phoneNumber').val()
+  console.log(id + '=' + password + "+" + name)
+  $.ajax({
+    type: "POST",
+    url: "/register",
+    cache: false,
+    data: {
+      "id": id,
+      "password": password,
+      "username": name,
+      "birthday": birthday,
+      "gender": gender,
+      "phoneNumber": phoneNumber
+    },
+    success: complete,
+    error: onError
+  });
 }
